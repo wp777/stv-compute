f5b6654fea2309009f35af0425518bcad570c791
Ubuntu <ubuntu@vps-2c19dab3.vps.ovh.net>
2024-05-14 01:21:30 +0000

    Joined new and old version

--- stv/gui_helper/asynchronous/domino_global.py
+++ stv/gui_helper/asynchronous/domino_global.py
@@ -19,15 +19,41 @@ if mode == "global":
         formula = re.findall("^FORMULA: .*", dstr, re.MULTILINE)[0].lstrip("FORMULA:")
 
     out_file = open("stv_output.txt", "w")
-    subprocess.call("../stv_v2/build/stv -f stv_model_file.txt -m 3 --OUTPUT_DOT_FILES", stdout=out_file, stderr=out_file, shell=True)
+    subprocess.call("../stv_v2/build/stv -f stv_model_file.txt -m 3 --OUTPUT_DOT_FILES --COUNTEREXAMPLE", stdout=out_file, stderr=out_file, shell=True)
     out_file.close()
 
     global_model = {"nodes": [], "links": []}
 
+    counter_example_states = []
+    counter_example_links = []
+    with open("stv_output.txt") as file:
+        states_line = False
+        links_line = False
+        for line in file:
+            if len(line.strip()) == 0:
+                states_line = False
+                links_line = False
+                continue
+            if states_line and "(" in line:
+                line = line.split("(")[-1]
+                line = line.strip().rstrip(";)")
+                for param in line.split(";"):
+                    counter_example_states[-1].append(param)
+            elif "States:" in line:
+                states_line = True
+                counter_example_states.append([])
+            elif links_line:
+                links_line = False
+                line = line.replace(";", ",")
+                line = line.strip().rstrip(",")
+                counter_example_links.append(line)
+            elif "Decisions:" in line:
+                links_line = True
 
 
     with open("dot/stv_model_file-GlobalModel.dot") as file:
         data = file.read().split("\n")[14:-1]
+        data = sorted(data, key=lambda x: (x.find('"->"'), x))
         states_ids = {}
         current_transition_id = 0
         current_id = 0
@@ -35,9 +61,16 @@ if mode == "global":
             if line.find('"->"') == -1:
                 elements = line.split('"')
                 node_id = elements[1]
+                node_id_value = int("1" + node_id.replace(";",""))
                 node_label = elements[3]
                 bgn = 1 if current_id == 0 else 0
                 strat = 0
+                for counter_ex in counter_example_states:
+                    strat = 0
+                    for param in counter_ex:
+                        if param not in line:
+                            strat = 0
+                    
                 global_model["nodes"].append({"T": {"id": node_id}, "id": current_id, "bgn": bgn, "str": strat})
                 states_ids[node_id] = current_id
                 current_id += 1
@@ -48,6 +81,9 @@ if mode == "global":
                 state_id = elements[1]
                 target_id = elements[3]
                 strat = 0
+                for counter_ex in counter_example_links:
+                    if counter_ex in line:
+                        strat = 0
                 global_model["links"].append({"id": transition_id, "source": states_ids[state_id], "target": states_ids[target_id], "T": [actions],
                      "str": strat})
                 current_transition_id += 1
--- stv/gui_helper/asynchronous/run_global.py
+++ stv/gui_helper/asynchronous/run_global.py
@@ -9,7 +9,7 @@ import re
 mode = sys.argv[3]  # "global" | "reduced"
 modelStr = sys.argv[4]
 
-if mode != "reduced":
+if mode != "reduced" and base64.b64decode(modelStr).decode("UTF-8")[0] != "%":
     with open("stv_model_file.txt", "w") as file:
         dstr = base64.b64decode(modelStr).decode("UTF-8")
         dstr = dstr.replace("\r", "")
@@ -26,8 +26,10 @@ if mode != "reduced":
     global_model = {"nodes": [], "links": []}
     # formula = "text"
 
+    
     with open("dot/stv_model_file-GlobalModel.dot") as file:
         data = file.read().split("\n")[14:-1]
+        data = sorted(data, key=lambda x: (x.find('"->"'), x))
         states_ids = {}
         current_transition_id = 0
         current_id = 0
@@ -35,6 +37,7 @@ if mode != "reduced":
             if line.find('"->"') == -1:
                 elements = line.split('"')
                 node_id = elements[1]
+                node_id_value = int("1" + node_id.replace(";", ""))
                 label_dict = {"id": node_id}
                 node_label = elements[3][1:-1]
                 for label in node_label.split("{"):
@@ -55,7 +58,7 @@ if mode != "reduced":
                 global_model["links"].append({"id": transition_id, "source": states_ids[state_id], "target": states_ids[target_id], "T": [actions],
                      "str": 0})
                 current_transition_id += 1
-    
+        
     for filename in os.listdir("dot"):
         f = os.path.join("dot", filename)
         if os.path.isfile(f) and "LocalModel" in filename:
@@ -74,8 +77,10 @@ if mode != "reduced":
                         label_dict = {"id": node_id}
                         node_label = elements[1][1:-1].split("|")[-1]
                         for param in node_label.split("\\n"):
-                            p_name, p_val = param.split("=")
-                            label_dict[p_name] = p_val
+                            if len(param.split("=")) == 2:
+                                p_name, p_val = param.split("=")
+                                label_dict[p_name] = p_val
+                        
                         bgn = 1 if node_id == 0 else 0
                         local_model["nodes"].append({"T": label_dict, "id": node_id, "bgn": bgn})
                     else:
@@ -89,7 +94,7 @@ if mode != "reduced":
                 localModels.append(json.dumps(local_model))
         if ".dot" in filename:
             os.remove(f)
-    
+
     with open("stv_debug.txt", "w") as file:
         print(json.dumps({
             "localModels": localModels,
